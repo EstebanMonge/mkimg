21afa19cbd6a7b98654baf2dd4cc6dc597aeeeb0
Esteban Monge <emonge@gbm.net>
2018-10-08 13:53:06 -0600

    VG display fixes on Debian based. Help, test mode, system tool detection in case that /usr/bin doesn't works

--- mkimg.sh
+++ mkimg.sh
@@ -1,12 +1,57 @@
 #!/bin/bash
 
-backup_path='/Backup_Linux/os_backups'
+### Print help
+function show_help () {
+        echo "Usage: $0 [ht] [p backup path]"
+        echo "t: test mode only print the final command"
+        echo "p: changes default backup path"
+        echo "h: print this help"
+        exit
+}
+
+### Determine if options are used
+
+while getopts p:ht option
+do
+case "${option}"
+in
+        p) backup_path=${OPTARG};;
+        t) test_flag=true;;
+        h) show_help
+esac
+done
+
+if [[ ! $backup_path ]]
+then
+        backup_path='/Backup_Linux/os_backups'
+fi
+
+
+### Determine tool paths
+if [[ -f /usr/bin/dd ]]
+then
+        dd_path=/usr/bin/dd
+else
+        dd_path=/bin/dd
+fi
+
+if [[ -f /usr/bin/gzip ]]
+then
+        gzip_path=/usr/bin/dd
+else
+        gzip_path=/bin/dd
+fi
+
+### Determine device to backup
 rootvg=$(df -PTh /| grep -oP '(?<=mapper/).*(?=-)')
+rootvg=${rootvg/--/-}
 hostname=$(hostname -s)
 if [[ ! -d $backup_path/$hostname ]]
 then
         mkdir $backup_path/$hostname
 fi
+
+### Start to backup device
 for partition in $(pvs|grep -w $rootvg|awk '{print $1}')
 do
         if [[ $partition == *mapper* ]]
@@ -17,6 +62,11 @@ do
         fi
         device=${partition/[[:digit:]]/}
         devicefile=${devicefile//[[:digit:]]/}
-        /usr/bin/dd if=$device conv=sync,noerror bs=64K | /usr/bin/gzip -c > $backup_path/$hostname/$devicefile.img.gz
-done
 
+        if [[ $test_flag ]]
+        then
+                echo "$dd_path if=$device conv=sync,noerror bs=64K | $gzip_path -c > $backup_path/$hostname/$devicefile.img.gz"
+        else
+                $dd_path if=$device conv=sync,noerror bs=64K | $gzip_path -c > $backup_path/$hostname/$devicefile.img.gz
+        fi
+done
